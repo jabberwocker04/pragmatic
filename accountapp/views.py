8934296151b6d114f7e30d27515bc40a0bcdbfe7
0000 <jabberwocker0@gmail.com>
2021-03-19 19:02:45 +0900

    djnago course 21 commit

--- accountapp/views.py
+++ accountapp/views.py
@@ -1,7 +1,12 @@
-from django.http import HttpResponse
+from django.contrib.auth.forms import UserCreationForm
+from django.contrib.auth.models import User
+from django.http import HttpResponse, HttpResponseRedirect
 from django.shortcuts import render
 
 # Create your views here.
+from django.urls import reverse, reverse_lazy
+from django.views.generic import CreateView
+
 from accountapp.models import HelloWorld
 
 
@@ -16,6 +21,18 @@ def hello_world(request):
         new_hello_world.save()
         # db에 데이터를 저장
 
-        return render(request, 'accountapp/hello_world.html', context={'hello_world_output': new_hello_world})
+        #list에 objects를 담아서 보내줄 예정임
+
+        return HttpResponseRedirect(reverse('accountapp:hello_world')) # reverse는 함수형 view에서 사용한다.
     else:
-        return render(request, 'accountapp/hello_world.html', context={'text': 'GET METHOD!!!'})
+        hello_world_list = HelloWorld.objects.all()
+        #get에서도 보내줄 예정임
+        return render(request, 'accountapp/hello_world.html', context={'hello_world_list': hello_world_list})
+
+
+
+class AccountCreateView(CreateView):
+    model = User
+    form_class = UserCreationForm
+    success_url = reverse_lazy('accountapp:hello_world') #reverse_lazy는 class형 view에서 사용한다
+    template_name = 'accountapp/create.html'
